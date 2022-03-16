bfd5dc406136a63fd845c5c37b260297ad26f16d
garlicGrape <sanchit809@gmail.com>
2022-03-15 21:12:39 -0400

    first commit

---
+++ notes3-15-22.js
@@ -0,0 +1,22 @@
+console.log("Yuh");
+
+/* 
+Each document or resource on the web is retrievable by a name. What is that name called, and what does the name consist of? →
+
+A URL… URLs consist of the follow parts:
+
+scheme/protocol - http
+domain or actual ip address - pizzaforyou.com
+port (optional) - 80 (default if http)
+path - /search
+query_string (optional) - ?type=vegan
+fragment_id (optional) - #topresult
+
+
+
+scheme://domain:port/path?query_string#fragment_id
+
+
+*/
+
+
